65619e55a53780697c2ea85043f96c680607bea1
muhammadikhwan309-ship-it <muhammadikhwan309@gmail.com>
2026-05-07 08:27:06 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,23 +1,12 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 19436,
-    // INI WAJIB ADA UNTUK AIVEN:
-    ssl: {
-        rejectUnauthorized: false
-    },
-    connectTimeout: 20000 // Tunggu 20 detik sebelum menyerah
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
     if (err) {
-        console.error('Koneksi Database Gagal total:', err.message);
+        console.error('Gagal konek:', err.message);
     } else {
-        console.log('ALHAMDULILLAH! Database Aiven Terhubung.');
+        console.log('✅ Database Aiven Terhubung!');
     }
 });
 
