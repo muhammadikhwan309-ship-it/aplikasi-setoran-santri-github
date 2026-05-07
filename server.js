fd3312a7470338248631e55afd3cbd919bea6167
muhammadikhwan309-ship-it <muhammadikhwan309@gmail.com>
2026-05-07 09:26:42 +0700

    Refactor server.js for clarity and structure

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db");
 const PDFDocument = require('pdfkit');
+const fs = require('fs');
 const path = require('path');
 const app = express();
 
